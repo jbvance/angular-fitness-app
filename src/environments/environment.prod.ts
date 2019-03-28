0f790a155bd07108389de66b694b96e0d401a741
Jason Vance <=>
2019-03-28 13:14:34 -0500

    update environment.prod.ts for production using firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyApzEc8RxJEHdNjgbXSyhhHDAY-cCGLq0g",
+    authDomain: "angular-workout-tracker.firebaseapp.com",
+    databaseURL: "https://angular-workout-tracker.firebaseio.com",
+    projectId: "angular-workout-tracker",
+    storageBucket: "angular-workout-tracker.appspot.com",
+    messagingSenderId: "563773782062"
+  }
 };
